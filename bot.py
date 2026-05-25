4aad8cc42b6a3b7e904cd7cdac47142eff131c23
mugidayo777-oss <mugidayo777@gmail.com>
2026-05-25 20:06:24 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,13 +1,14 @@
+import os
 import discord
 from discord.ext import commands
 
-TOKEN = "あとで入れる"
+TOKEN = os.getenv("DISCORD_TOKEN")
 
 intents = discord.Intents.default()
 bot = commands.Bot(command_prefix="!", intents=intents)
 
 @bot.event
 async def on_ready():
-    print("tikubi bot 起動")
+    print(f"{bot.user} 起動しました")
 
 bot.run(TOKEN)
