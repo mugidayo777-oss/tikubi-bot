551fbfe6298135081b6a120ccccffd7961b79011
mugidayo777-oss <mugidayo777@gmail.com>
2026-05-25 19:40:42 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,13 @@
-print("tikubi bot 準備OK")
+import discord
+from discord.ext import commands
+
+TOKEN = "あとで入れる"
+
+intents = discord.Intents.default()
+bot = commands.Bot(command_prefix="!", intents=intents)
+
+@bot.event
+async def on_ready():
+    print("tikubi bot 起動")
+
+bot.run(TOKEN)
